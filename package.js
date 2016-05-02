80a4d12a69fccf8b18eafb4baf76d485b152ecfd
Pauli B <pauli.buccini@gmail.com>
2016-05-02 18:59:21 +0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tapfuse:offline-collection',
-  version: '1.7.2',
+  version: '1.7.3',
   // Brief, one-line summary of the package.
   summary: 'Not yet released',
   // URL to the Git repository containing the source code for this package.
