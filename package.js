895733bf3a2708e2ae52d3a5ee75f12e6bfdb17f
Pauli B <pauli.buccini@gmail.com>
2016-11-23 16:05:39 +0700

    added console

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tapfuse:offline-collection',
-  version: '7.0.2',
+  version: '7.0.3',
   // Brief, one-line summary of the package.
   summary: 'Not yet released',
   // URL to the Git repository containing the source code for this package.
