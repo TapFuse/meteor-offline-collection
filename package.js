94086682166fb1c6a3d601cb676dda267a1e3cca
Po Buchi <po@ox2.io>
2017-10-03 17:32:42 +0700

    v9.5.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tapfuse:offline-collection',
-  version: '9.4.0',
+  version: '9.5.0',
   // Brief, one-line summary of the package.
   summary: 'Not yet released',
   // URL to the Git repository containing the source code for this package.
